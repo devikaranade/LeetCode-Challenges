2d1a1a4715e11b3ae71e37a7ce1db655c229b2c9
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2024-02-12 14:10:50 -0800

    Time: 42 ms (31.26%) | Memory: 16.5 MB (75.23%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,23 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        '''
+        if n==1 -> [[1]]
+        if n==2 -< [[1], [1,1]]
+        if n==3 -> [[1], [1,1], []]
+        '''
+        res = [1]
+        final = [res]
+        if numRows<1:
+            return []
+        for i in range(1, numRows):
+            prev_row = final[i-1]
+            curr_row = [1]
+
+            for j in range(1,i):
+                curr_row.append(prev_row[j-1]+prev_row[j])
+            curr_row.append(1)
+            final.append(curr_row)
+        
+        return final 
+
+        

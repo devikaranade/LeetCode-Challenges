1b8df44d2c927fabac288c38198b9da985a9cf35
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2024-02-16 14:00:14 -0800

    Time: 724 ms (25.64%) | Memory: 21 MB (96.55%) - LeetSync

---
+++ 816-design-hashset/design-hashset.py
@@ -0,0 +1,23 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.MyHashSet = []
+        
+    def add(self, key: int) -> None:
+        if not self.contains(key):
+            self.MyHashSet.append(key)
+        
+    def remove(self, key: int) -> None:
+        if self.contains(key):
+            self.MyHashSet.remove(key)
+
+    def contains(self, key: int) -> bool:
+        if key in self.MyHashSet:
+            return True
+        return False
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)

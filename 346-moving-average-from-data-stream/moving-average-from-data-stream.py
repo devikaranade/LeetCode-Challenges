efb8b8c9794eda0e3948211f304f5029b8fec253
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2024-02-23 18:53:15 -0800

    Time: 99 ms (10.90%) | Memory: 19.9 MB (51.02%) - LeetSync

--- 346-moving-average-from-data-stream/moving-average-from-data-stream.py
+++ 346-moving-average-from-data-stream/moving-average-from-data-stream.py
@@ -7,22 +7,13 @@ class MovingAverage:
         
         
 
-    def next(self, val: int) -> float:
-        '''
-        window = [1, 11, 3]
-        total = 0+1 = 1+ 10 = 11+3 = 14
-        return 1/1 = 1.0 = 14/3 = 4.666
-
-        '''
-        
+    def next(self, val: int) -> float:  
         if len(self.window)>=self.size: 
             self.total-=self.window.popleft()
         self.total += val
         self.window.append(val)
         return self.total/len(self.window)
     
-
-        
 
 
 # Your MovingAverage object will be instantiated and called as such:

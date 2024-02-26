7fd2e7697875f43ff8b1ed15d1aca39f334ce392
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2024-02-26 09:58:40 -0800

    Time: 350 ms (78.79%) | Memory: 17.9 MB (74.10%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -7,20 +7,3 @@ class Solution:
             if i+nums[i]>=last:
                 last = i
         return last==0
-            
-
-        
-        
-        # while i<=len(nums)-1:
-        #     if nums[i]>steps:
-        #         steps=nums[i] #steps = 0->2 -> 3 -> 4
-        #     steps = steps-1  # i = 2 -> 3
-        #     if steps<0:
-        #         return False
-        #     i+=1
-        # return True
-
-
-            
-                
-        

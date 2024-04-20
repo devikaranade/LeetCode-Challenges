456fe21ddda7aa45ced4bfffdbfee8fef5250da0
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2024-04-19 19:08:29 -0700

    Time: 3016 ms (5.00%) | Memory: 19.9 MB (85.97%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.py
+++ 303-range-sum-query-immutable/range-sum-query-immutable.py
@@ -2,7 +2,7 @@ class NumArray:
 
     def __init__(self, nums: List[int]):
         self.nums = nums
-
+    @lru_cache
     def sumRange(self, left: int, right: int) -> int:
         self.total = 0
         for i in range(left, right+1):

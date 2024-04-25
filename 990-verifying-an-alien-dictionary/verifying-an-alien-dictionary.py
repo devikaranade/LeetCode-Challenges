c9020e0b24a58520d409157c1efa9e618b904be6
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2024-04-25 16:40:17 -0700

    Time: 46 ms (20.27%) | Memory: 16.5 MB (74.91%) - LeetSync

---
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        order_map = {char: i for i, char in enumerate(order)}
+        for i in range(1, len(words)):
+            word1=words[i-1]
+            word2=words[i]
+            min_len = min(len(word1), len(word2))
+            for j in range(min_len):
+                if word1[j]!=word2[j]:
+                    if order_map[word1[j]]>order_map[word2[j]]:
+                        return False
+                    break
+            else:
+                if len(word1)>len(word2):
+                    return False
+        return True

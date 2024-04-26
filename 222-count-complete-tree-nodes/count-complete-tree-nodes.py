8ff5f671b5f304b3ffff7e8b48467b4f62987d40
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2024-04-25 18:20:30 -0700

    Time: 54 ms (69.15%) | Memory: 21.6 MB (97.68%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.py
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.py
@@ -6,23 +6,39 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        if not root.right and not root.left:
-            return 1
-        elif root.right is None:
-            return 2
-        left = self.level(root.left)
-        right = self.level(root.right)
-        if left==right:
-            return 2**left + self.countNodes(root.right)
-        else:
-            return 2**right + self.countNodes(root.left)
+        def dfs(root):
+            if not root:
+                return 0
+            nonlocal x
+            x+=1
+            dfs(root.left)
+            dfs(root.right)
+        x=0
+        dfs(root)
+        return x
 
-    def level(self, root):
-        i=0
-        while root:
-            i+=1
-            root=root.left
-        return i
+
+
+
+
+
+    #     if not root:
+    #         return 0
+    #     if not root.right and not root.left:
+    #         return 1
+    #     elif root.right is None:
+    #         return 2
+    #     left = self.level(root.left)
+    #     right = self.level(root.right)
+    #     if left==right:
+    #         return 2**left + self.countNodes(root.right)
+    #     else:
+    #         return 2**right + self.countNodes(root.left)
+
+    # def level(self, root):
+    #     i=0
+    #     while root:
+    #         i+=1
+    #         root=root.left
+    #     return i
         

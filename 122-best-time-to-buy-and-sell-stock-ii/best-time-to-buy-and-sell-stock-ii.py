61d28de622df56a0ea54c0816b6aa65668967b34
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2024-05-29 11:10:52 -0700

    Time: 63 ms (24.65%) | Memory: 17.8 MB (35.00%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -3,5 +3,5 @@ class Solution:
         profit = 0
         for i in range(1, len(prices)):
             if prices[i]>prices[i-1]:
-                profit += prices[i]-prices[i-1] # profit = 4
+                profit += prices[i]-prices[i-1] 
         return profit

161bfe836252678f0779c15f8964bc34ee1062f3
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2024-10-20 15:32:50 -0700

    Time: 0 ms (100.00%) | Memory: 18.3 MB (82.82%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        res = []
+        visited = [0]*numCourses
+
+        if numCourses==1:
+            return [0]
+        
+        G = defaultdict(list)
+        for src,des in prerequisites:
+            G[des].append(src)
+        
+        def dfs(courses):
+            if visited[courses]==-1:
+                return True
+            if visited[courses]==1:
+                return False
+            visited[courses]=-1
+
+            for neighbor in G[courses]:
+                if dfs(neighbor):
+                    return True
+            visited[courses]=1
+            res.append(courses)
+            return False   
+
+        for c in range(numCourses):
+            if dfs(c):
+                return []
+        return res[::-1]
+ 

c668fc884325cc24e190e0df6045d3579f6db359
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2024-10-30 23:56:01 -0700

    Time: 3 ms (33.33%) | Memory: 17.2 MB (32.58%) - LeetSync

---
+++ 544-output-contest-matches/output-contest-matches.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findContestMatch(self, n: int) -> str:
+        j = n
+        i = 1
+        pairs = []
+        while i<j:
+            pairs.append((i,j))
+            i+=1
+            j-=1
+        print(pairs)
+
+        while len(pairs)>1:
+            tmp = []
+            while len(pairs)>0:
+                front=pairs.pop(0)
+                back = pairs.pop()
+                tmp.append((front,back))
+            pairs=tmp
+        return str(pairs[0]).replace(" ","") 
+        
+
+

53e87e465d9c4f6910cbaaf642a56a2ee0064fdc
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2024-10-31 21:29:26 -0700

    Time: 4 ms (20.73%) | Memory: 18.1 MB (5.04%) - LeetSync

--- 545-boundary-of-binary-tree/boundary-of-binary-tree.py
+++ 545-boundary-of-binary-tree/boundary-of-binary-tree.py
@@ -6,10 +6,6 @@
 #         self.right = right
 class Solution:
     def boundaryOfBinaryTree(self, root: Optional[TreeNode]) -> List[int]:
-        # leafs = []
-        # left_bound = []
-        # right_bound = []
-        # root_lst = [root.val]
         def dfs_left(root):
             if root:
                 if not root.left and not root.right:
@@ -42,27 +38,9 @@ class Solution:
                         return dfs_right(root.left)+[root.val]
             else:
                 return []
-
-
-
-
-            # if not root:
-            #     return 
-            # if root.left:
-            #     if not root.right and not root.left:
-            #         leafs.append(root.val)
-            #     left_bound.append(root.left.val)
-            #     dfs_left(root.left)
-            # if root.right:
-            #     if not root.right and not root.left:
-            #         leafs.append(root.val)
-                # dfs_left(root.right)
-        
-        # def dfs_right(root):
             
         boundaries = [root.val] + dfs_left(root.left)+ find_leaves(root.left)+find_leaves(root.right)+dfs_right(root.right)
         
-        # dfs_left(root)
         return boundaries
 
 

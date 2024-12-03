fd30f46220a1ab07055581ad8c742947c67a8a1f
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2024-12-03 15:46:32 -0800

    Time: 234 ms (71.81%) | Memory: 19.3 MB (48.56%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,32 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        queue = deque()
-        count = 0
         m = len(grid)
         n = len(grid[0])
-        directions = [(-1,0),(0,-1),(1,0),(0,1)]
+        dir_ = [(1,0),(0,1),(-1,0),(0,-1)]
+        count = 0
 
-        def bfs(r,c):
-            queue = deque()
-            queue.append((r,c))
-            while queue:
-                row, col = queue.popleft()
-                for dr,dc in directions:
-                    r,c = row+dr, col+dc
-                    if 0<=r<m and 0<=c<n and grid[r][c]=="1":   
-                        queue.append((r,c))
-                        grid[r][c]="0"
-
-
-        
-    
+        def bfs(i,j):
+            q = deque()
+            q.append((i,j))
+            while q:
+                row, col = q.popleft()
+                for r,c in dir_:
+                    nr = row+r
+                    nc = col+c
+                    if 0<=nr<m and 0<=nc<n and grid[nr][nc]=="1":  
+                        q.append((nr,nc))
+                        grid[nr][nc]="0"
+                
         for r in range(m):
             for c in range(n):
                 if grid[r][c]=="1":
                     bfs(r,c)
                     count+=1
         return count
-                
-        
-        
-        

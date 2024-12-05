09a8341b57dec52bf1bc967832c5022a034ba343
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2024-12-04 20:12:02 -0800

    Time: 6 ms (26.97%) | Memory: 17.4 MB (16.12%) - LeetSync

---
+++ 604-design-compressed-string-iterator/design-compressed-string-iterator.py
@@ -0,0 +1,31 @@
+class StringIterator:
+
+    def __init__(self, compressedString: str):
+        self.s = deque(compressedString)
+
+    def next(self) -> str:
+        while self.s:
+            k = self.s.popleft()
+            n = ""
+            while self.s and self.s[0].isdigit():
+                n+=self.s.popleft()
+            n = int(n)
+            if n!=1:
+                self.s.appendleft(str(n-1))
+                self.s.appendleft(k)
+            return k
+        else:
+            return " "
+
+
+        
+
+    def hasNext(self) -> bool:
+        return len(self.s)>1
+        
+
+
+# Your StringIterator object will be instantiated and called as such:
+# obj = StringIterator(compressedString)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()

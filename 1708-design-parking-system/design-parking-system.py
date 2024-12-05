30b43b8321105fbd61484df285645b2ef1c636b7
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2024-12-04 19:27:36 -0800

    Time: 14 ms (56.70%) | Memory: 17.8 MB (8.65%) - LeetSync

--- 1708-design-parking-system/design-parking-system.py
+++ 1708-design-parking-system/design-parking-system.py
@@ -1,20 +1,16 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        self.space = {
-            1: big,
-            2: medium,
-            3: small
-        }
-        
+        self.big=big
+        self.medium=medium
+        self.small = small
+        self.empty = [big,medium,small]
 
     def addCar(self, carType: int) -> bool:
-        if not self.space.get(carType):
-            return False
-        self.space[carType]-=1
-        return True
-
-
+        if self.empty[carType-1]>0:
+            self.empty[carType-1]-=1
+            return True
+        return False
         
 
 

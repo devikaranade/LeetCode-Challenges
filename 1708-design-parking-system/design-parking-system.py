222acb172fcc12769543b267c8470c52a1764d4e
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2024-12-04 19:29:10 -0800

    Time: 9 ms (81.61%) | Memory: 18 MB (5.53%) - LeetSync

--- 1708-design-parking-system/design-parking-system.py
+++ 1708-design-parking-system/design-parking-system.py
@@ -4,15 +4,14 @@ class ParkingSystem:
         self.big=big
         self.medium=medium
         self.small = small
-        self.empty = [big,medium,small]
+        self.empty = {1:big, 2:medium, 3:small}
 
     def addCar(self, carType: int) -> bool:
-        if self.empty[carType-1]>0:
-            self.empty[carType-1]-=1
+        if self.empty[carType]>0:
+            self.empty[carType]-=1
             return True
         return False
         
-
 
 # Your ParkingSystem object will be instantiated and called as such:
 # obj = ParkingSystem(big, medium, small)

2633199033180243d8de1b3913b298fb160b724c
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2024-12-12 13:15:36 -0800

    Time: 3 ms (81.58%) | Memory: 18.2 MB (8.88%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -2,23 +2,9 @@ class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         h = {}
         for i in range(len(numbers)):
-            diff = target-numbers[i] # 9-2 = 7
+            diff = target-numbers[i] 
             if diff not in h:
-                h[numbers[i]] = i+1 #h[0] = 2
+                h[numbers[i]] = i+1 
             else:
                 return h[diff], i+1
-        
-
-
-
-        # for i in range(0, len(numbers)):
-        #     for j in range(i+1, len(numbers)):
-        #         if numbers[i]+numbers[j]==target:
-        #             print(i,j)
-        #             return [i+1, j+1]
-                
-            
-            
-
-
         

17b478c6fc472b02fb5d6b930386a2ac0340b24c
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2024-12-28 11:19:41 -0800

    Time: 55 ms (45.02%) | Memory: 38.2 MB (9.90%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        mod = 0
+        d = {0:-1}
+        for i in range(len(nums)):
+            mod = (mod + nums[i])%k
+            if mod not in d:
+                d[mod]=i
+            else:
+                if i-d[mod]>1:
+                    return True
+        return False

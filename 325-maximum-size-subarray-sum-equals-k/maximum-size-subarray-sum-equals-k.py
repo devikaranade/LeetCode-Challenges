8da0d0050b72de3f9e74630ce8aff63aa57e489a
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2025-05-13 22:47:41 -0400

    Time: 151 ms (32.11%) | Memory: 62.9 MB (94.17%) - LeetSync

--- 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.py
+++ 325-maximum-size-subarray-sum-equals-k/maximum-size-subarray-sum-equals-k.py
@@ -1,14 +1,15 @@
 class Solution:
     def maxSubArrayLen(self, nums: List[int], k: int) -> int:
-        d = {0:1}
-        total = 0
-        max_length = 0
+        d = {}
+        count = 0
+        psum = 0
+
         for i, num in enumerate(nums):
-            total+=num
-            if total == k:
-                max_length = i + 1
-            if total-k in d:
-                max_length = max(max_length, i-d[total-k])
-            if total not in d:
-                d[total]=i
-        return max_length
+            psum+=num
+            if psum==k:
+                count=i+1
+            if psum-k in d:
+                count = max(count, i-d[psum-k])
+            if psum not in d:
+                d[psum]=i
+        return count

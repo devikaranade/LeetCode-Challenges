8e0b4ac0f32312039a0919bd527e0d85e740c1ed
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2025-05-26 23:15:20 -0400

    Time: 10 ms (87.38%) | Memory: 21.3 MB (43.88%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,9 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        d = defaultdict(list)
-        for i in strs:
-            k = str(sorted(i))
-            if k not in d:
-                d[k]=[]
-            d[k].append(i)
-        return list(d.values())
+        map_strs=defaultdict(list)
+        res = []
+        for s in strs:
+            sorted_s = tuple(sorted(s))
+            map_strs[sorted_s].append(s)
+        for v in map_strs.values():
+            res.append(v)
+        return res

561e0d5a0fce52036f6e68180de8b98705fe9543
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2025-06-08 12:48:44 -0400

    Time: 3 ms (84.82%) | Memory: 19.1 MB (24.03%) - LeetSync

--- 800-letter-case-permutation/letter-case-permutation.py
+++ 800-letter-case-permutation/letter-case-permutation.py
@@ -2,13 +2,12 @@ class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
         ans = []
         def rec(ip, op):
-            nonlocal ans
             if ip=="":
                 ans.append(op)
-                return
+                return ans
             if ip[0].isalpha():
-                op1 = op + ip[0].lower()
-                op2 = op + ip[0].upper()
+                op1 = op+ip[0].lower()
+                op2 = op+ip[0].upper()
                 rec(ip[1:], op1)
                 rec(ip[1:], op2)
             else:

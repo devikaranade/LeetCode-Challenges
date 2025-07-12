1ada71b661cb5c6fa7067f20d390f1d6ae282a8f
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2025-07-12 09:58:05 -0400

    Time: 5 ms (34.92%) | Memory: 17.8 MB (44.20%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,12 +1,12 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         q = deque()
+        fresh = 0
         time = 0
+        dir_ = [(0,1),(1,0),(0,-1),(-1,0)]
+
         m = len(grid)
         n = len(grid[0])
-        fresh = 0
-        dir_ = [(0,1),(1,0),(-1,0), (0,-1)]
-
         for r in range(m):
             for c in range(n):
                 if grid[r][c]==2:
@@ -18,12 +18,11 @@ class Solution:
             for _ in range(len(q)):
                 r,c = q.popleft()
                 for dr,dc in dir_:
-                    nr = r+dr
-                    nc = c+dc
+                    nr=r+dr
+                    nc=c+dc
                     if 0<=nr<m and 0<=nc<n and grid[nr][nc]==1:
                         grid[nr][nc]=2
                         fresh-=1
                         q.append((nr,nc))
             time+=1
-            
         return time if fresh==0 else -1

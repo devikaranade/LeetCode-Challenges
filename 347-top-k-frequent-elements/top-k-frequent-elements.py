00fa45e7efd73791ae5d1081b7342eec466095ad
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2025-07-29 22:39:05 -0400

    Time: 5 ms (61.58%) | Memory: 21.3 MB (62.00%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,7 +1,5 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        #Counter = {1:3, 2:2, 3:1}
-        #min_heap = [(-3:1),(-2:2)]
         c = Counter(nums)
         heap = []
         for key,val in c.items():

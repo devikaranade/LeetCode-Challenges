3d49370397a5b15ae0eeeed6cb9a4bbd01756f5c
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2025-08-06 22:15:26 -0400

    Time: 47 ms (25.94%) | Memory: 30.9 MB (66.59%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -10,28 +10,3 @@ class Solution:
             if nums[i-1]>0:
                 res.append(i)
         return res
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # res = []
-        # for i in range(len(nums)):
-        #     new_index = abs(nums[i])-1
-        #     if nums[new_index]>0:
-        #         nums[new_index]*=-1
-        
-        # for i in range(1, len(nums)+1):
-        #     if nums[i-1]>0:
-        #         res.append(i)
-
-        # return res
-

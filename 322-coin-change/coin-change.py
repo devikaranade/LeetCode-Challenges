19ae5605f30972ddc4f87e44b2a04b18b9413821
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2025-08-11 19:40:37 -0400

    Time: 722 ms (74.67%) | Memory: 18 MB (87.69%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,19 +1,9 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount==0:
-            return 0
-        q = deque([amount])
-        seen = set()
-        count=0
-        while q:
-            for _ in range(len(q)):
-                curr_amt = q.popleft()
-                for coin in coins:   
-                    diff_amt = curr_amt - coin
-                    if diff_amt>0 and diff_amt not in seen:
-                        seen.add(diff_amt)
-                        q.append(diff_amt)
-                    if diff_amt==0:
-                        return count+1
-            count+=1
-        return -1
+        dp = [float('inf')] * (amount+1)
+        dp[0]=0
+
+        for coin in coins:
+            for x in range(coin, amount+1):
+                dp[x] = min(dp[x], dp[x-coin]+1)
+        return dp[amount] if dp[amount]!=float('inf') else -1

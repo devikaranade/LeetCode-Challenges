7cfde5ffb3e46132ea9fc67da42db503cfc6b88f
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2025-08-27 21:07:42 -0400

    Time: 83 ms (34.74%) | Memory: 23 MB (28.89%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,16 +1,12 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         heap = []
-        for i in points:
-            xi = i[0]*i[0]
-            yi = i[1]*i[1]
-            eucledian_dist = -(xi+yi) # sqrt(10)
-            heapq.heappush(heap, (eucledian_dist, i))
+        for x,y in points:
+            eucledian = -((x*x)+(y*y))
+            heapq.heappush(heap, (eucledian, [x,y]))
             if len(heap)>k:
                 heapq.heappop(heap)
         l = []
         for k,v in heap:
             l.append([v[0],v[1]])
         return l
-
-

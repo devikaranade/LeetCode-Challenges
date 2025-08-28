81abdb749738b15c1c2071fb232bb1083dc539ac
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2025-08-27 21:44:16 -0400

    Time: 6 ms (55.15%) | Memory: 21.4 MB (39.58%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,6 +1,5 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # h = {1:3, 2:2, 3:1}
         h = Counter(nums)
         heap = []
         for key,val in h.items():

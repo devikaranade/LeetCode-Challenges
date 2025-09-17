e2d7a578248b224c47d3eb200e89b08cc64a3a8f
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2025-09-17 17:57:16 -0400

    Time: 11 ms (90.61%) | Memory: 18.4 MB (89.42%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -4,6 +4,7 @@ class Solution:
         if len(nums)==1:
             return True
         for i in range(len(nums)-1, -1, -1):
-            if i+nums[i]>=last:
-                last =  i 
+            if nums[i]+i>=last:
+                last = i 
         return last==0
+

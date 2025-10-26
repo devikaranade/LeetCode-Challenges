392b2c30b67ddb184dc9fa446299e1addc496b76
Devika Ranade <41178447+devikaranade@users.noreply.github.com>
2025-10-26 16:38:06 -0400

    Time: 4 ms (26.83%) | Memory: 42.1 MB (39.99%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -11,14 +11,17 @@ class Solution {
         Stack<Character> stack=new Stack<>();
         
         for (int i=0;i<s.length();i++) {
-            if (map.containsKey(s.charAt(i))){
-                stack.push(s.charAt(i));
+            char c = s.charAt(i);
+
+            if (map.containsKey(c)){
+                stack.push(c);
             }
             else{
                 if (stack.isEmpty()){
                     return false;
                 }
-                if (map.get(stack.pop()) != s.charAt(i)) {
+                char ch = stack.pop();
+                if (map.get(ch) != s.charAt(i)) {
                     return false;
                 }
             }
